1eab04b4e0a2dc810548b2767b5217a3b40ee644
Abdelrahman Farghali <darkchaker@gmail.com>
2022-11-28 01:59:45 +0200

    recreated folder to suit the first collection, deleted result files edited according to searching for cid on ipfs

--- metadata.py
+++ metadata.py
@@ -14,8 +14,8 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
 # Base metadata. MUST BE EDITED.
-BASE_IMAGE_URL = "ipfs://<-- Your CID Code-->"
-BASE_NAME = ""
+BASE_IMAGE_URL = "ipfs://"#<-- Your CID Code-->"
+BASE_NAME = "metadata.json"
 
 BASE_JSON = {
     "name": BASE_NAME,
